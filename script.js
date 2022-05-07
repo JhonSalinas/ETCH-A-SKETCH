3f18e186c7c0076eb2ca862d09670e9f8b63187b
JhonSalinas <js.jhonsalinas@gmail.com>
2022-05-06 20:58:09 -0500

    grid ended

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-
+document.querySelector('body').setAttribute('style', 'margin: 0');
+//THIS CREATED AND PUSH THE GRID
 const body = document.querySelector('body');
 
 const container = document.createElement('div');
 container.classList.add('container');
+
 //that's created 16x16 divs
 for (let i = 0; i < 16 * 16; i++) {
     const divs = document.createElement('div');
+    divs.setAttribute('style','background-Color: red; height: 100px; border: solid 1px black;');
     container.appendChild(divs);
 }
 
 document.querySelector('body').appendChild(container);
+
+//HERE IS THE CODE FOR the STYLES 
+container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16 , 1fr)');
