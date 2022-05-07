53782963b65702820ef6cde2e9ed54207fdc1cd8
JhonSalinas <js.jhonsalinas@gmail.com>
2022-05-06 22:50:39 -0500

    painted funcionality is update

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 document.querySelector('body').setAttribute('style', 'margin: 0');
 //THIS CREATED AND PUSH THE GRID
 const body = document.querySelector('body');
-
 const container = document.createElement('div');
+
 container.classList.add('container');
 
 //that's created 16x16 divs
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < 256; i++) {
     const divs = document.createElement('div');
-    divs.setAttribute('style','background-Color: red; height: 100px; border: solid 1px black;');
+    divs.classList.add('child');
+    divs.setAttribute('style',' height: 100px; border: solid 1px black;');
     container.appendChild(divs);
+    // console.log(divs);
 }
-
+// document.createStyleSheet().addRule('.child', 'background-Color: red; height: 100px; border: solid 1px black;');
 document.querySelector('body').appendChild(container);
 
 //HERE IS THE CODE FOR the STYLES 
 container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16 , 1fr)');
+
+//this is for the pencil efect
+const allDivs = document.querySelectorAll('.child');
+//this add the class white at the div that is clicked
+allDivs.forEach(element => element.addEventListener('click', event => {
+    event.target.classList.add('painted');
+    // event.target.classList.setAttribute('style','background-Color: white;');
+    console.log(event.target);
+    event.target.setAttribute('style', 'background-Color: red');
+}));
